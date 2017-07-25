4b8c702b7415c538b14f0fda743f1fadd993d27a
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2017-07-25 22:28:13 +0900

    wip

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,10 @@ module WebpackerActioncable
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # initializerなどではrakeタスク実行時に反映されないのでここで
+    if ENV['DOCKER']
+      config.paths.add 'config/database', with: 'config/database.docker.yml'
+    end
   end
 end
